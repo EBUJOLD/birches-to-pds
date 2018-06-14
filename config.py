1ae25c6eb7eebbeb6a4ebd52b08281e326d4ac34
Unknown <emilyebujold@gmail.com>
2018-06-14 15:07:56 -0700

    Created Empty config file
    modified some comments in main and created config file

---
+++ config.py
@@ -0,0 +1,11 @@
+#################################
+# Config file for birches-to-pds program
+# Written by Emily Bujold and Sarah Wilczewski
+#################################
+
+# AIT unwraps data and outputs files. birches-to-pds reads this data in. 
+# Where is the source data?
+file_source = PUT SOMETHING HERE
+
+# Where do you want the output of birches-to-pds to go?
+file_destination = PUT SOMETHING HERE
